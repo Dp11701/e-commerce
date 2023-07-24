73b9346a8db3353e603c9da33fa58f974111b23e
Dphu11701 <dphu569@gmail.com>
2023-07-24 15:53:18 +0700

    fix babel/plugin-proposal

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Outlet, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home, Login, SignUp, Account, DetailsPages } from "./pages";
 import { useSelector } from "react-redux";
 
--- src/containers/Details/index.tsx
+++ src/containers/Details/index.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Footer from "../../components/Layout/Footer";
 import Header from "../../components/Layout/Header";
-import styles from "./style.module.css";
 
 const DetailsPages: React.FC = () => {
   return (
